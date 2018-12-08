ff9a6de1fa88e4094eaf68b0a765bd69cca07d56
Ishwar Jangid <ishwarjangid@Ishwars-MacBook-Air.local>
2018-12-08 16:14:34 +0530

    tree

---
+++ scripts/tree.py
@@ -0,0 +1,16 @@
+from sklearn import tree
+
+
+x = [[180, 111, 21], [111, 111, 33]]
+
+
+y = ['male', 'female']
+
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(x, y)
+
+prediction = clf.predict([[122, 232, 44]])
+
+print prediction
